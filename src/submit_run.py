11f4590c79ce80b673d5c13a3f8130514e7c1997
Tiriele <147039063+Tiriele@users.noreply.github.com>
2025-09-27 10:03:20 +0000

    Deleted test line

--- src/submit_run.py
+++ src/submit_run.py
@@ -2,8 +2,6 @@
 import kfp
 import sys
 
-# Testing 27.9.2025 12:50
-
 sys.path.append('../src')
 from pipelines.pipeline_definitions.pipeline_definition import pipeline
 from pipelines.pipeline_arg.pipeline_arg import arguments
